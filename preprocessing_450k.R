10e3cdf18c1db21638807aa20cf8ddfc838a6574
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-08 11:06:16 +0800

    Update preprocessing_450k.R

--- preprocessing_450k.R
+++ preprocessing_450k.R
@@ -2,6 +2,7 @@ setwd("/root/TCGA/Rnbeads")
 suppressMessages(library(RnBeads))
 options(bitmapType="cairo")
 options(scipen=999)
+suppressMessages(library(IlluminaHumanMethylation450kmanifest))
 
 ## preprocessing
 #idat files
