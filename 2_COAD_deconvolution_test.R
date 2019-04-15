77e92beab987103fe8f6d3b289e7f9ba4c56be34
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-04-16 00:32:47 +0800

    Create 2_COAD_deconvolution_test.R

---
+++ 2_COAD_deconvolution_test.R
@@ -0,0 +1,5 @@
+suppressMessages(library(RnBeads))
+options(bitmapType="cairo")
+options(scipen=999)
+rnb.set.norm=load.rnb.set("/root/TCGA/Rnbeads/COAD/RnBeads_normalization/rnb.set.norm_withNormal.RData.zip")
+
