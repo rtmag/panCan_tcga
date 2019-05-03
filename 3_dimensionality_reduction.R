671f54323f51c5f806bf0ff2282535c603384f24
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-03 09:56:38 +0800

    Update 3_dimensionality_reduction.R

--- 3_dimensionality_reduction.R
+++ 3_dimensionality_reduction.R
@@ -21,3 +21,44 @@ res<-NbClust(mval.sig, distance = "euclidean",method = "kmeans")
 saveRDS(res,"res.rds")
 
 KMeansSparseCluster(x, K=)
+
+####################################################################
+
+mval = readRDS("mval_TUMORonly_tumor_vs_normal_FDR5p.rds")
+
+mval = mval[complete.cases(mval), ]
+
+pdf("pca_parameters.pdf")
+par(mfrow=c(2,2))
+
+ir.pca <- prcomp(t(mval),
+                 center = FALSE,
+                 scale. = FALSE) 
+sx=summary(ir.pca)
+plot(ir.pca$x[,1],ir.pca$x[,2],xlab=paste("PC1:",round(sx$importance[2,1]*100,digits=1),"%"),
+     ylab=paste("PC2:",round(sx$importance[2,2]*100,digits=1),"%"),pch=19)
+#########
+ir.pca <- prcomp(t(mval),
+                 center = FALSE,
+                 scale. = TRUE) 
+sx=summary(ir.pca)
+plot(ir.pca$x[,1],ir.pca$x[,2],xlab=paste("PC1:",round(sx$importance[2,1]*100,digits=1),"%"),
+     ylab=paste("PC2:",round(sx$importance[2,2]*100,digits=1),"%"),pch=19)
+#########
+ir.pca <- prcomp(t(mval),
+                 center = TRUE,
+                 scale. = FALSE) 
+sx=summary(ir.pca)
+plot(ir.pca$x[,1],ir.pca$x[,2],xlab=paste("PC1:",round(sx$importance[2,1]*100,digits=1),"%"),
+     ylab=paste("PC2:",round(sx$importance[2,2]*100,digits=1),"%"),pch=19)
+#########
+ir.pca <- prcomp(t(mval),
+                 center = TRUE,
+                 scale. = TRUE) 
+sx=summary(ir.pca)
+plot(ir.pca$x[,1],ir.pca$x[,2],xlab=paste("PC1:",round(sx$importance[2,1]*100,digits=1),"%"),
+     ylab=paste("PC2:",round(sx$importance[2,2]*100,digits=1),"%"),pch=19)
+#########
+dev.off()
+library(umap)
+
