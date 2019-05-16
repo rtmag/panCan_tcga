586b2d4ca4a8c6c289a03ecf30393f57f1e0b51d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-17 03:33:52 +0800

    Create meme-chip.bash

---
+++ meth_TF/meme-chip.bash
@@ -0,0 +1,5 @@
+mkdir hi_tumor
+~/myPrograms/MEME/scripts/meme-chip hi_tumor_cpg_y757_20190516_212414.fasta -oc hi_tumor/
+
+mkdir hi_normal
+~/myPrograms/MEME/scripts/meme-chip hi_normal_cpg_g697_20190516_212244.fasta -oc hi_normal/
