ae1e1a5e873db7778bb1b2f62c62927c67e0a7e9
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-16 15:17:08 +0800

    Update diff_meth.R

--- meth_TF/diff_meth.R
+++ meth_TF/diff_meth.R
@@ -15,3 +15,26 @@ dmc <- rnb.execute.computeDiffMeth(rnb.set.norm,pheno.cols=c("Tumor"))
 comparison <- get.comparisons(dmc)[1]
 dmc_table <-get.table(dmc, comparison, "sites", return.data.frame=TRUE)
 #
+table(abs(dmc_table$mean.diff)>.25 & dmc_table$diffmeth.p.adj.fdr<0.05)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
+meth.norm = meth(rnb.set.norm)
+meth.norm.sig=meth.norm[abs(dmc_table$mean.diff)>.25 & dmc_table$diffmeth.p.adj.fdr<0.05,]
+
+track= TUMOR 
+track[track=="TUMOR"]=1
+track[track=="NORMAL"]=2
+track=as.numeric(track)
+colores=c("red","white")
+clab=as.character(colores[track])
+
+png("heatmap_diff_TUMOR_VS_NORMAL_FDR5p.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="CpGs",key.title="Methylation lvl",ColSideColors=clab)
+legend("topright",legend=c("TUMOR","NORMAL"),fill=c("red","white"), border=T, bty="n" )
+dev.off()
+##########
