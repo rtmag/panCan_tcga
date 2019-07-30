ac62e2db00a25cb40259c097e17142e3d4bcc971
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-30 08:25:50 +0800

    Update 1_TCGA_DIFF_TUMOR_NORMAL_METH.R

--- meth_TF/1_TCGA_DIFF_TUMOR_NORMAL_METH.R
+++ meth_TF/1_TCGA_DIFF_TUMOR_NORMAL_METH.R
@@ -6,6 +6,9 @@ library(gplots)
 library(factoextra)
 library(RColorBrewer)
 
+num.cores <- 20
+parallel.setup(num.cores)
+
 normal_sample_number <- read.table(
          pipe('grep -c "NORMAL" /root/TCGA/Rnbeads/*/*_info_withNormal.csv|perl -pe "s/\\//\t/g"|perl -pe "s/\\:/\t/g"|cut -f5,7'),
          stringsAsFactors=FALSE)
@@ -16,6 +19,30 @@ for(i in 1:length(normal_sample_number)){
      tcga.dir <- list.dirs(path = "/root/TCGA/Rnbeads/", full.names = TRUE, recursive = FALSE)
      command <- paste0("/root/TCGA/Rnbeads/",normal_sample_number[i],"/RnBeads_normalization/rnb.set.norm_withNormal.RData.zip")
      rnb.set.norm=load.rnb.set(command)
+         
+     command <- paste0("/root/TCGA/Rnbeads/",normal_sample_number[i],"/",normal_sample_number[i],"_TP53_mutation_info_withNormal.csv")
+     TUMOR = read.csv(command,header=TRUE)
+     TUMOR = as.character(TUMOR$Variant_Classification)
+     TUMOR[TUMOR!="NORMAL"] = "TUMOR"
+     rnb.set.norm@pheno = data.frame(rnb.set.norm@pheno, Tumor = TUMOR)
+         
+     rnb.set.filtered <- rnb.execute.sex.removal(rnb.set.norm)$dataset
+     rnb.set.filtered <- rnb.execute.snp.removal(rnb.set.filtered, snp="any")$dataset
+     rnb.set.filtered <- rnb.execute.na.removal(rnb.set.filtered)$dataset
+         
+     #save filtered object
+         #make dir
+     command = paste0("mkdir /root/TCGA/TF_METH/",normal_sample_number[i])
+     system(command)
+     command = paste0("/root/TCGA/TF_METH/",normal_sample_number[i]"/rnb.set.norm.filtered.RData")
+     save.rnb.set(rnb.set.filtered,path=command)
 
+     dmc <- rnb.execute.computeDiffMeth(rnb.set.filtered, pheno.cols=c("Tumor") )
+     comparison <- get.comparisons(dmc)[1]
+     print(comparison)
+     dmc_table <-get.table(dmc, comparison, "sites", return.data.frame=TRUE)
+         
+     command = paste0("/root/TCGA/TF_METH/",normal_sample_number[i],"/",normal_sample_number[i],"_dmc_table.csv")
+     write.csv(dmc_table,command)
 }
 
