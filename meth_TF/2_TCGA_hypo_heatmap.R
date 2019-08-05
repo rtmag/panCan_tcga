2ad2a3a93108035ca4aa2fc7d16398f737da3456
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-08-06 00:42:07 +0800

    Update 2_TCGA_hypo_heatmap.R

--- meth_TF/2_TCGA_hypo_heatmap.R
+++ meth_TF/2_TCGA_hypo_heatmap.R
@@ -14,10 +14,46 @@ tcga.dir <- list.dirs(path = "/root/TCGA/TF_METH/", full.names = TRUE, recursive
 for(i in 1:length(tcga.dir)){
     # get name of TCGA
     tcga_name <- gsub(".+\\/\\/","",tcga.dir[i],perl=TRUE)
+    
+    # read rdata
+    path <- paste0(tcga.dir[i],"/","rnb.set.norm.filtered.RData.zip")
+    rnb.set.norm=load.rnb.set(path)
+
+    
+    
     # hypo hyper cpg
     path <- paste0(tcga.dir[i],"/",tcga_name,"_dmc_table.csv")
     meth_table <- read.csv(path)
-    # read rdata
+        #Read Annotation
+        library("IlluminaHumanMethylation450kanno.ilmn12.hg19")
+        data("IlluminaHumanMethylation450kanno.ilmn12.hg19")
+        annotation.table = getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+    
+    hi_normal = meth.norm[dmc_table$mean.diff>.25 & dmc_table$diffmeth.p.adj.fdr<0.05,]
+    hi_normal= hi_normal[complete.cases(hi_normal),]
+    hi_normal= hi_normal[apply(hi_normal,1,sd)>0,]
+
+    hi_tumor = meth.norm[dmc_table$mean.diff<(-.25) & dmc_table$diffmeth.p.adj.fdr<0.05,]
+    hi_tumor= hi_tumor[complete.cases(hi_tumor),]
+    hi_tumor= hi_tumor[apply(hi_tumor,1,sd)>0,]
+
+    hi_tumor_anno = annotation.table[rownames(annotation.table) %in% rownames(hi_tumor),c(1,2,2)]
+    path <- paste0(tcga.dir[i],"/",tcga_name,"_hi_tumor_cpg.bed")
+    write.table(hi_tumor_anno, path,sep="\t",quote=F,row.names=F,col.names=F)
+    hi_tumor_anno[,2] = hi_tumor_anno[,2]-50
+    hi_tumor_anno[,3] = hi_tumor_anno[,3]+50
+    path <- paste0(tcga.dir[i],"/",tcga_name,"_hi_tumor_cpg_100bp.bed")
+    write.table(hi_tumor_anno, path,sep="\t",quote=F,row.names=F,col.names=F)
+
+    hi_normal_anno = annotation.table[rownames(annotation.table) %in% rownames(hi_normal),c(1,2,2)]
+    path <- paste0(tcga.dir[i],"/",tcga_name,"_hi_normal_cpg.bed")
+    write.table(hi_normal_anno, path,sep="\t",quote=F,row.names=F,col.names=F)
+    hi_normal_anno[,2] = hi_normal_anno[,2]-50
+    hi_normal_anno[,3] = hi_normal_anno[,3]+50
+    path <- paste0(tcga.dir[i],"/",tcga_name,"_hi_normal_cpg_100bp.bed")
+    write.table(hi_normal_anno, path,sep="\t",quote=F,row.names=F,col.names=F)
+
+    
     
     #do heatmap
     
