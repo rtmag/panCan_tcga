9f53d671bee4c8d7c773c6a369a3533c69254b4a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-08-07 17:06:27 +0800

    Update 3_annotation_annotationPlots.R

--- meth_TF/3_annotation_annotationPlots.R
+++ meth_TF/3_annotation_annotationPlots.R
@@ -6,3 +6,27 @@ library(factoextra)
 library(RColorBrewer)
 
 tcga.dir <- list.dirs(path = "/root/TCGA/TF_METH/", full.names = TRUE, recursive = FALSE)
+
+
+for(i in 1:length(tcga.dir)){
+    # get name of TCGA
+    tcga_name <- gsub(".+\\/\\/","",tcga.dir[i],perl=TRUE)
+  
+    # annotate hi norm cpg
+    command <- paste0("awk '{print $1\"\t\"$2-1\"\t\"$3}' ",
+                      paste0(tcga.dir[i],"/",tcga_name,"_hi_normal_cpg.bed|"),
+                      "annotatePeaks.pl - hg19 -annStats ",
+                      paste0(tcga.dir[i],"/",tcga_name,"_hi_normal_cpg.annStats > "),
+                      paste0(tcga.dir[i],"/",tcga_name,"_hi_normal_cpg.anno")
+                     )        
+    system(command)
+
+    # annotate hi tumor cpg
+    command <- paste0("awk '{print $1\"\t\"$2-1\"\t\"$3}' ",
+                      paste0(tcga.dir[i],"/",tcga_name,"_hi_tumor_cpg.bed|"),
+                      "annotatePeaks.pl - hg19 -annStats ",
+                      paste0(tcga.dir[i],"/",tcga_name,"_hi_tumor_cpg.annStats > "),
+                      paste0(tcga.dir[i],"/",tcga_name,"_hi_tumor_cpg.anno")
+                     )        
+    system(command)
+  }
